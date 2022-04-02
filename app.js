efeec7f8b1113297570ea9b1c2d5050ded1d7465
nadav1996700 <nadav1996700@gmail.com>
2022-04-02 17:44:18 +0300

    3 bugs left - lost message, result change, and create random equation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var equation = "6*2-9/3";
 var result = 9;
 
 /* set the result */
-//document.getElementById("result").innerHTML = '   ' + result;
+//document.getElementById('result').innerHTML = '   ' + result;
 
 /* handle enter click */
 function on_click_enter() {
@@ -15,13 +15,43 @@ function on_click_enter() {
         if(!paint_squares()) {
             first_index_of_current_line += 7
             focus_box = first_index_of_current_line;
-            if(first_index_of_current_line > 42) {
+            if(first_index_of_current_line > 36) {
                 GameOver('l'); // lost
             }
         } else {
             GameOver('w'); // won
         }
     }
+}
+
+function GameOver(result) {
+    var box_one = document.getElementById('box-result-1');
+    var box_two = document.getElementById('box-result-2');
+    var box_three = document.getElementById('box-result-3');
+    var box_four = document.getElementById('box-result-4');
+    const boxes = new Array(box_one, box_two, box_three, box_four);
+    if(result === 'l') {
+        document.getElementById("text_result").innerHTML = "The equation was " + equation;
+        boxes.forEach(b => {
+            b.style.background = "#FF0000";
+            b.style.borderColor = "#FF0000";
+        });
+        box_one.innerHTML = 'L';
+        box_two.innerHTML = 'O';
+        box_three.innerHTML = 'S';
+        box_four.innerHTML = 'T';
+    } else {
+        boxes.forEach(b => {
+            b.style.background = "#90EE90";
+            b.style.borderColor = "#90EE90";
+        });
+        box_one.innerHTML = 'W';
+        box_two.innerHTML = 'O';
+        box_three.innerHTML = 'N';
+        box_four.innerHTML = '!';
+    }
+    document.getElementById("buttons").style.visibility = 'hidden';
+    document.getElementById("result_message").style.visibility = 'visible';
 }
 
 // paint the squares in the right color and check if the player won the game 
@@ -32,14 +62,24 @@ function paint_squares() {
         var box = document.getElementById('box-' + eval(first_index_of_current_line + i));
         if(box.innerHTML === equation[i]) {
             box.style.backgroundColor = "#90EE90"; // paint in green
+            box.style.color = "#FFFFFF";
+            box.style.borderColor = "#90EE90";
             paint_button(box.innerHTML, "#90EE90");
         }
         else {
             isWin = false;
             if(equation.includes(box.innerHTML)) {
                 box.style.backgroundColor = "#CCCC00"; // paint in yellow
+                box.style.color = "#FFFFFF";
+                box.style.borderColor = "#CCCC00";
                 paint_button(box.innerHTML, "#CCCC00");
-            }   
+            } 
+            else {
+                box.style.backgroundColor = "#343a40"; // paint in black
+                box.style.color = "#FFFFFF";
+                box.style.borderColor = "#343a40";
+                paint_button(box.innerHTML, "#343a40");
+            }  
         }
     }
     return isWin ? true : false;
@@ -66,6 +106,7 @@ function paint_button(sign, color) {
             break;
     }
     button.style.backgroundColor = color;
+    button.style.borderColor = color;
 }
 
 /* check if the equasion is equal to the require result */
@@ -76,10 +117,6 @@ function checkEquation() {
         my_equation += document.getElementById("box-" + i).innerHTML;
     }
     return eval(my_equation) === result ? true : false;
-}
-
-function GameOver() {
-
 }
 
 /* delete value of box and decrease focus_box */
